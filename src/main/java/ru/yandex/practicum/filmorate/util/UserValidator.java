f6b012d494f1929e9ab243ce9154479ab572cd09
Arctiс16 <artemmalikov2004@gmail.com>
2024-03-20 18:43:16 +0300

    Refactor 7

--- src/main/java/ru/yandex/practicum/filmorate/util/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/UserValidator.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class UserValidator {
     public static boolean validate(User user) throws ValidationException {
-        if(user.getLogin().split(" ").length != 1 || user.getLogin().isBlank()) {
+        if (user.getLogin().split(" ").length != 1 || user.getLogin().isBlank()) {
             throw new ValidationException("Логин не может быть пустым или содержать пробелы!");
         } else if (user.getBirthday().isAfter(LocalDate.now())) {
             throw new ValidationException("Дата рождения не может быть в будущем!");
