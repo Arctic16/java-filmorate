0e4be32231e412d2c13e024332eb41d1123b7454
Arctiс16 <artemmalikov2004@gmail.com>
2024-03-20 18:48:35 +0300

    Refactor 8

--- src/test/java/ru/yandex/practicum/filmorate/UserValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidatorTest.java
@@ -13,44 +13,44 @@ public class UserValidatorTest {
 
     @Test
     void addCorrectUserTest() throws ValidationException {
-        User user = new User(1,"adsas@asda","Arct",""
-                , LocalDate.of(2000,10,10));
+        User user = new User(1,"adsas@asda","Arct","",
+                LocalDate.of(2000,10,10));
         Assertions.assertEquals(true, UserValidator.validate(user));
     }
     @Test
     void addIncorrectEmailUserTest() {
-        User user = new User(1,"adsasasda","Arct",""
-                , LocalDate.of(2000,10,10));
-        ValidationException exception = Assertions.assertThrows(ValidationException.class
-                , () -> UserValidator.validate(user));
-        Assertions.assertEquals("Почта не может быть пустой и не содержать символа @ !"
-                , exception.getMessage());
+        User user = new User(1,"adsasasda","Arct","",
+                LocalDate.of(2000,10,10));
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+                () -> UserValidator.validate(user));
+        Assertions.assertEquals("Почта не может быть пустой и не содержать символа @ !",
+                exception.getMessage());
     }
     @Test
     void addIncorrectBirthdayUserTest() {
-        User user = new User(1,"adsas@asda","Arct",""
-                , LocalDate.of(2100,10,10));
-        ValidationException exception = Assertions.assertThrows(ValidationException.class
-                , () -> UserValidator.validate(user));
-        Assertions.assertEquals("Дата рождения не может быть в будущем!"
-                , exception.getMessage());
+        User user = new User(1,"adsas@asda","Arct","",
+                LocalDate.of(2100,10,10));
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+                () -> UserValidator.validate(user));
+        Assertions.assertEquals("Дата рождения не может быть в будущем!",
+                exception.getMessage());
 
     }
     @Test
     void addIncorrectLoginUserTest() {
-        User user1 = new User(1,"adsas@asda","Ar ct",""
-                , LocalDate.of(2000,10,10));
-        User user2 = new User(1,"adsas@asda","",""
-                , LocalDate.of(2000,10,10));
+        User user1 = new User(1,"adsas@asda","Ar ct","",
+                LocalDate.of(2000,10,10));
+        User user2 = new User(1,"adsas@asda","","",
+                LocalDate.of(2000,10,10));
 
-        ValidationException exception = Assertions.assertThrows(ValidationException.class
-                , () -> UserValidator.validate(user1));
-        ValidationException exception1 = Assertions.assertThrows(ValidationException.class
-                , () -> UserValidator.validate(user2));
-        Assertions.assertEquals("Логин не может быть пустым или содержать пробелы!"
-                , exception.getMessage());
-        Assertions.assertEquals("Логин не может быть пустым или содержать пробелы!"
-                , exception1.getMessage());
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+                () -> UserValidator.validate(user1));
+        ValidationException exception1 = Assertions.assertThrows(ValidationException.class,
+                () -> UserValidator.validate(user2));
+        Assertions.assertEquals("Логин не может быть пустым или содержать пробелы!",
+                exception.getMessage());
+        Assertions.assertEquals("Логин не может быть пустым или содержать пробелы!",
+                exception1.getMessage());
 
 
     }
