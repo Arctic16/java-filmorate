5d05a16b91e73aa7f47df1fd988a06264d7ad5cc
Arctiс16 <artemmalikov2004@gmail.com>
2024-03-21 19:13:42 +0300

    Refactor: обновление фильмов и пользователей

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.util.FilmValidator;
 import java.util.ArrayList;
@@ -18,6 +19,9 @@ public class FilmController {
     @PutMapping("/films")
     public Film updateFilm(@RequestBody Film film) {
         log.info("Получен PUT запрос.");
+        if(!filmHashMap.containsKey(film.getId())){
+            throw new ValidationException("Фильма с таким id не существует");
+        }
         if (FilmValidator.validate(film) && filmHashMap.containsKey(film.getId())) {
             filmHashMap.put(film.getId(), film);
             log.info("Фильм обновлён!");
