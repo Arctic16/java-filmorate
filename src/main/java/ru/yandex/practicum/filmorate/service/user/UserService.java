54c0a146ae78993db2e7bab5fa1aa64d81f8faaf
Arctiс16 <artemmalikov2004@gmail.com>
2024-05-13 21:31:42 +0300

    Codestyle 7

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.util.HashSet;
 import java.util.Set;
+
 @Service
 public class UserService {
 
@@ -21,23 +22,25 @@ public class UserService {
             userStorage.getUserById(user2Id).getFriends().add(user1Id);
         }
     }
+
     public void removeFriends(int user1Id, int user2Id) {
         if(userStorage.getUserById(user1Id) != null && userStorage.getUserById(user2Id).getFriends() != null) {
             userStorage.getUserById(user1Id).getFriends().remove(user2Id);
             userStorage.getUserById(user2Id).getFriends().remove(user1Id);
         }
     }
+
     public Set<Integer> getFriends(int userId) {
         return userStorage.getUserById(userId).getFriends();
     }
 
-    public Set<Integer> getCommonFriends(int user1Id, int user2Id ){
+    public Set<Integer> getCommonFriends(int user1Id, int user2Id) {
         Set<Integer> common = new HashSet<>(userStorage.getUserById(user1Id).getFriends());
         common.retainAll(userStorage.getUserById(user2Id).getFriends());
         return common;
     }
+
     public UserStorage getUserStorage() {
         return userStorage;
     }
-
 }
