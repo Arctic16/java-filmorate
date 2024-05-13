25daa5faff1ad75e0e088442d5cebd3978a5a759
Arctiс16 <artemmalikov2004@gmail.com>
2024-05-14 00:48:59 +0300

    Refactor

--- src/main/java/ru/yandex/practicum/filmorate/MyExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/MyExceptionHandler.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.ResponseErrorHandler;
 import ru.yandex.practicum.filmorate.exceptions.SearchException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
@@ -14,20 +16,22 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 public class MyExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Error> handleValidationException(final ValidationException ex) {
         log.error("Получен код 400");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(ex.getMessage()));
     }
 
     @ExceptionHandler(SearchException.class)
-    public ResponseEntity<String> handleSearchException(SearchException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Error> handleSearchException(SearchException ex) {
         log.error("Получен код 404");
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Error(ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+    public ResponseEntity<Error> handleException(Exception ex) {
         log.error("Получен код 500");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Error(ex.getMessage()));
     }
 }
