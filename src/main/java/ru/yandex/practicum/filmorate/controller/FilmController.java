a7f155a91eb559662fdc4f5b8af64f07656348b0
Arctiс16 <artemmalikov2004@gmail.com>
2024-05-14 21:38:31 +0300

    Codestyle 13

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
 import ru.yandex.practicum.filmorate.service.user.UserService;
-import ru.yandex.practicum.filmorate.util.FilmValidator;
 import java.util.List;
 
 @RestController
